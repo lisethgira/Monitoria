827bb7b22f707201f9864cd2ab6c5de24cbb5962
LuDev01 <luisatoro0121@hotmail.com>
2023-10-16 16:56:49 -0500

    Real State WebPage

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import Companies from "./Components/Companies";
+import CustomButton from "./Components/CustomButton";
+import Hero from "./Components/Hero";
+import Navbar from "./Components/Navbar";
+import Guide from "./Components/Guide";
+import Properties from "./Components/Properties";
+import Details from "./Components/Details";
+import GetStarted from "./Components/GetStarted";
+import Footer from "./Components/Footer";
+
+function App() {
+  return (
+  <>
+    <Hero/>
+    <Companies/>
+    <Guide/>
+    <Properties/>
+    <Details/>
+    <GetStarted/>
+    <Footer/>
+  
+  </>
+  );
+}
+
+
+export default App;
